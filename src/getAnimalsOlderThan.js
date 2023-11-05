61084442cd1f93fac8d2c8d2a5542012b8e2e2ce
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-05 18:53:41 +0000

    cria e aprova requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => data.species
-.find(({ name }) => name === animal)
-.residents.every((resident) => resident.age >= age);
+  .find(({ name }) => name === animal)
+  .residents.every((resident) => resident.age >= age);
 
 console.log(getAnimalsOlderThan('penguins', 10));
 
