14b91d616a4668819dfa973e808a96c1e8cacae7
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-05 19:29:15 +0000

    cria e aprova requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (employeeName !== undefined) {
+    return data.employees.find((employee) =>
+      employee.firstName === employeeName
+      || employee.lastName === employeeName);
+  }
+  return {};
 };
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
