5bbe8cad8ec7529553b1793c459853ecc9765a52
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-07 00:09:21 +0000

    cria e aprova requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+const countAnimals = (animal = {}) => {
+  const completeList = data.species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents
+      .filter((residente) => (!animal.sex || residente.sex === animal.sex)).length;
+    return acc;
+  }, {});
+
+  if (!animal.species) {
+    return completeList;
+  }
+  return completeList[animal.species];
 };
+
+// console.log(countAnimals());
+// console.log(countAnimals({ species: 'penguins' }));
+// console.log(countAnimals({ species: 'bears', sex: 'male' }));
 
 module.exports = countAnimals;
