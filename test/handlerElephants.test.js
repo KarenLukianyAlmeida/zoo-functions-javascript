b4424bad8d6e13f933ea08a4279e1d571255e5c5
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-07 11:55:35 +0000

    cria segunbo e terceiro teste do requisto 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -6,4 +6,14 @@ describe('Testes da função HandlerElephants', () => {
     const expected = undefined;
     expect(actual).toBe(expected);
   });
+  it('ao chamar a função handlreElephantes com parametro diferente de string, retorna a mensagem "Parâmetro inválido, é necessário uma string"', () => {
+    const actual = handlerElephants(10);
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+  it('ao chamar a função handlreElephantes com parametro "count", retorna o número inteiro 4', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
 });
