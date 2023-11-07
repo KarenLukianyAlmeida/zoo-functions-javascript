9f5f0fca8d236cf6c146a75ddac6e75b15b4b908
Karen Almeida <karenalmeida.lukiany@gmail.com>
2023-11-07 22:49:09 +0000

    cria e aprova requisto 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const person = data.employees.find((employee) => employee.id === id);
+  const firstSpeciesId = person.responsibleFor[0];
+  const foundSpecies = data.species.find((species) => species.id === firstSpeciesId);
+  const oldestAnimal = foundSpecies.residents
+    .reduce((prev, curr) => (prev.age > curr.age ? prev : curr));
+  const reportOldestAnimal = [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+
+  return reportOldestAnimal;
 };
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
